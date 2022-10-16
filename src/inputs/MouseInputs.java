6e37866035ac398240a2f3c5c00e2422079fe644
Artem Ivanov <arti.ivnv@gmail.com>
2022-10-16 22:30:41 +0300

    Basic mouse and keyboard inputs added

---
+++ src/inputs/MouseInputs.java
@@ -0,0 +1,64 @@
+package src.inputs;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+import src.main.GamePanel;
+
+// mouseListener() -> clicked, pressed, released
+// mouseMotionListener() -> moved, dragged
+
+public class MouseInputs implements MouseListener, MouseMotionListener {
+
+    private GamePanel gamePanel;
+
+    public MouseInputs(GamePanel gamePanel) {
+        this.gamePanel = gamePanel;
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        System.out.println("Mouse moving!");
+        gamePanel.setRectPosition(e.getX(), e.getY());
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        System.out.println("Click");
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
